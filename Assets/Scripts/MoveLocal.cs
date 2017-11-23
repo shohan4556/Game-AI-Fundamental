d03baae26b1395632d4b2bca55a1862aeb34ad34
Shohanur Rahaman <shohan4556@gmail.com>
2017-11-23 23:19:13 +0600

    clair model configured

---
+++ Assets/Scripts/MoveLocal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLocal : MonoBehaviour {
+
+	public Transform target;
+	public float speed = 1f;
+	public float accuracy = 1f;
+	public float rotSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
