2a11817e4a036a85a8c65d75cbaad277a5d158dd
Shohanur Rahaman <shohan4556@gmail.com>
2017-11-26 18:40:57 +0600

    working with standard asset script

--- Assets/Scripts/WaypointFollow.cs
+++ Assets/Scripts/WaypointFollow.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class WaypointFollow : MonoBehaviour {
 
-	public GameObject[] waypoints;
+	public UnityStandardAssets.Utility.WaypointCircuit circuit;
+	//public GameObject[] waypoints;
 	private int currentWaypointID = 0;
 	public float moveSpeed = 1f;
 	public float rotSpeed = 1f;
@@ -12,21 +15,22 @@ public class WaypointFollow : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+		//waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+		//	Array.Sort(waypoints);
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		if(waypoints.Length == 0) return;
+		if(circuit.Waypoints.Length == 0) return;
 
-		Vector3 lookAtGoal = new Vector3(waypoints[currentWaypointID].transform.position.x, this.transform.position.y, waypoints[currentWaypointID].transform.position.z);
+		Vector3 lookAtGoal = new Vector3(circuit.Waypoints[currentWaypointID].transform.position.x, this.transform.position.y, circuit.Waypoints[currentWaypointID].transform.position.z);
 		Vector3 direction = lookAtGoal - this.transform.position;
 		// rotation 
 		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime*rotSpeed);
 
 		if(direction.magnitude < reachDist){
 			currentWaypointID++;
-			if(currentWaypointID>=waypoints.Length){
+			if(currentWaypointID >= circuit.Waypoints.Length){
 				currentWaypointID = 0;
 			}
 		}
