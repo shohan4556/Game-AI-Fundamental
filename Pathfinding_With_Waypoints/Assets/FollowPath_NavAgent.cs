dc23a03e07f85bc15a8c25a0cce44415bcc2d894
Shohanur Rahaman <shohan4556@gmail.com>
2017-12-01 11:21:01 +0600

    click to move with navmesh agent

--- Pathfinding_With_Waypoints/Assets/FollowPath_NavAgent.cs
+++ Pathfinding_With_Waypoints/Assets/FollowPath_NavAgent.cs
@@ -8,7 +8,8 @@ public class FollowPath_NavAgent : MonoBehaviour {
 	public GameObject wpManager;
 	public GameObject[] wps; 
 	NavMeshAgent agent;
-
+	private Vector3 dest;
+	bool canSetDest = true;
 	// Use this for initialization
 	void Start () {
 		wps = wpManager.GetComponent<WaypointManager>().Waypoints;
@@ -24,6 +25,37 @@ public class FollowPath_NavAgent : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetMouseButton(0)){
+			Vector3 point = Input.mousePosition;
+			
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(point);
+			if(Physics.Raycast(ray, out hit) && canSetDest){
+				dest = hit.point;
+				agent.SetDestination(dest);
+				canSetDest = false;
+
+				//Debug.DrawRay(Camera.main.transform.position, dest, Color.red);
+			}
+			
+		}
+
+		if(agent.remainingDistance <= 0.1){
+			print("Path Completed");
+			canSetDest = true;
+		}
+		print(dest + "remaining distance : "+agent.remainingDistance);
 	}
+
+	/// <summary>
+	/// Callback to draw gizmos that are pickable and always drawn.
+	/// </summary>
+	void OnDrawGizmos()
+	{
+		if(dest!=null){
+			Debug.DrawLine(Camera.main.transform.position, dest, Color.black);
+			Debug.Log("draw ray pos : "+dest);
+		}
+	}
+
 }
