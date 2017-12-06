1b6399632d49080ffbee191d620aaf711cf76356
Shohanur Rahaman <shohan4556@gmail.com>
2017-12-06 11:15:20 +0600

    crowd simulation

---
+++ NavMeshAgent/Assets/AIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour {
+
+	public Transform Goal;
+	private NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(Goal.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ NavMeshAgent/Assets/CrowdControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CrowdControl : MonoBehaviour {
+
+	GameObject[] goalLocations;
+	NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+		goalLocations = GameObject.FindGameObjectsWithTag("Goal");
+		agent = GetComponent<NavMeshAgent>();
+		Vector3 rndDest = goalLocations[Random.Range(0, goalLocations.Length)].transform.position;
+		agent.SetDestination(rndDest);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(agent.remainingDistance < 1){
+			Vector3 rndDest = goalLocations[Random.Range(0, goalLocations.Length)].transform.position;
+			agent.SetDestination(rndDest);
+		}
+	}
+}
