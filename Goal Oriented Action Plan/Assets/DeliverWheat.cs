273e16da8b72aa8434bc47286f2d155af03d1247
Shohanur Rahaman <shohan4556@gmail.com>
2017-12-25 22:28:11 +0600

    GOAP final

---
+++ Goal Oriented Action Plan/Assets/DeliverWheat.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliverWheat : GoapAction {
+
+	bool completed = false;
+	float startTime = 0;
+	public float workDuration = 2; // seconds
+	public Inventory inv;
+	
+	public DeliverWheat () {
+		addPrecondition ("hasWheat", true); 
+		addEffect ("doJob", true);
+		name = "Harvest";
+	}
+	
+	public override void reset ()
+	{
+		completed = false;
+		startTime = 0;
+	}
+	
+	public override bool isDone ()
+	{
+		return completed;
+	}
+	
+	public override bool requiresInRange ()
+	{
+		return true; 
+	}
+	
+	public override bool checkProceduralPrecondition (GameObject agent)
+	{	
+		return true;
+	}
+	
+	public override bool perform (GameObject agent)
+	{
+		if (startTime == 0)
+		{
+			Debug.Log("Starting: " + name);
+			startTime = Time.time;
+		}
+
+		if (Time.time - startTime > workDuration) 
+		{
+			Debug.Log("Finished: " + name);
+			inv.flourLevel ++;
+			completed = true;
+		}
+		return true;
+	}
+	
+}
--- Goal Oriented Action Plan/Assets/Worker.cs
+++ Goal Oriented Action Plan/Assets/Worker.cs
@@ -8,6 +8,7 @@ public class Worker : MonoBehaviour, IGoap
 	NavMeshAgent agent;
 	Vector3 previousDestination;
 	Inventory inv;
+	public Inventory windmill;
 
 	void Start()
 	{
@@ -18,7 +19,9 @@ public class Worker : MonoBehaviour, IGoap
 	public HashSet<KeyValuePair<string,object>> GetWorldState () 
 	{
 		HashSet<KeyValuePair<string,object>> worldData = new HashSet<KeyValuePair<string,object>> ();
+		worldData.Add(new KeyValuePair<string, object>("hasStock", (windmill.flourLevel > 4) ));
 		worldData.Add(new KeyValuePair<string, object>("hasFlour", (inv.flourLevel > 1) ));
+		worldData.Add(new KeyValuePair<string, object>("hasDelivery", (inv.breadLevel > 4) ));		
 		return worldData;
 	}
 
@@ -26,6 +29,8 @@ public class Worker : MonoBehaviour, IGoap
 	public HashSet<KeyValuePair<string,object>> CreateGoalState ()
 	{
 		HashSet<KeyValuePair<string,object>> goal = new HashSet<KeyValuePair<string,object>> ();
+		goal.Add(new KeyValuePair<string, object>("doJob", true ));
+
 		return goal;
 	}
 
